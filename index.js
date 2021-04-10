cc8e14d9d8788ac8e5d4d30cebeb709161ad9b04
nivaniuc <nivaniucc@gmail.com>
2021-04-09 21:45:58 -0400

    Dear future self--I just made what I want the template to look like. If something is broken, refer back to this first. Thanks.

---
+++ index.js
@@ -0,0 +1,33 @@
+const template = `# ${title}
+
+${description}
+
+## TABLE OF CONTENTS ##
+
+> [Installation] (#INSTALLATION)
+
+> [Usage] (#USAGE)
+
+> [License] (#LICENSE)
+
+> [Credits] (#CREDITS)
+
+> [Contact] (#CONTACTS)
+
+#INSTALLATION
+${installation}
+
+#USAGE
+${usage}
+
+#LICENSE
+${license}
+
+#CREDITS
+${credits}
+
+#CONTACTS
+-GitHub : ${github}
+-LinkedIn : ${linkedin}
+-Email : ${email}
+`
